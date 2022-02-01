25c4944cb79cb9f5b251b4728dbb62d838ab42ce
shaastar <mshamailhsiraj@gmail.com>
2022-02-01 18:28:35 +0530

    translate progress pipe issue

--- src/app/pages/language/language.page.ts
+++ src/app/pages/language/language.page.ts
@@ -1,15 +1,71 @@
 import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
+import { TranslateConfigService } from '../../services/translate-config.service';
 @Component({
   selector: 'app-language',
   templateUrl: './language.page.html',
   styleUrls: ['./language.page.scss'],
 })
 export class LanguagePage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  selectedLanguage: string = "ENGLISH";
+  constructor(
+    private actionSheet: ActionSheetController,
+    private translateConfigService: TranslateConfigService
+  ) {
+    // this.selectedLanguage = this.translateConfigService.getDefaultLanguage();
   }
 
+  selectLanguage(language: string) {
+    this.translateConfigService.setLanguage(language);
+  }
+
+  ngOnInit() {}
+
+  openSelectLanguage() {
+    this.actionSheet
+      .create({
+        // header: 'Albums',
+        cssClass: 'my-custom-class',
+        mode: 'md',
+        buttons: [
+          {
+            text: 'English',
+            icon: 'share',
+            handler: () => {
+              console.log('English Selected');
+              this.selectedLanguage = "ENGLISH"
+              this.selectLanguage('en');
+            },
+          },
+          {
+            text: 'Spanish',
+            icon: 'caret-forward-circle',
+            handler: () => {
+              console.log('Spanish Selected');
+              this.selectedLanguage = "SPANISH"
+              this.selectLanguage('sp');
+            },
+          },
+          // {
+          //   text: 'Favorite',
+          //   icon: 'heart',
+          //   handler: () => {
+          //     console.log('Favorite clicked');
+          //   },
+          // },
+          // {
+          //   text: 'Cancel',
+          //   icon: 'close',
+          //   role: 'cancel',
+          //   handler: () => {
+          //     console.log('Cancel clicked');
+          //   }
+          // }
+        ],
+      })
+      .then((res) => {
+        res.present();
+      });
+  }
 }
