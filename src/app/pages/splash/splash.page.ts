9219953ec9166211b696739a92ed4c52756901b7
Rasheek <shrasheek@gmail.com>
2022-02-08 17:23:43 +0530

    Point select, splash fixes

--- src/app/pages/splash/splash.page.ts
+++ src/app/pages/splash/splash.page.ts
@@ -10,8 +10,8 @@ export class SplashPage implements OnInit {
   constructor(private router: Router) {}
 
   ngOnInit() {
-    // setTimeout(() => {
-    //   this.router.navigate(['tabs/point-select']);
-    // }, 5000);
+    setTimeout(() => {
+      this.router.navigate(['tabs/point-select']);
+    }, 4000);
   }
 }
